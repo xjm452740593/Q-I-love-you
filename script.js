d284f88b44edf91fe3185ff9f305b363ddf45bfe
xjm452740593 <52527476+xjm452740593@users.noreply.github.com>
2020-02-14 08:03:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const words = [
-    '我爱你', 'I Love You', 'Σ\'αγαπώ', ' أنا أحبك', 'Ma armastan sind',
+    '我爱你', 'I Love You', 'Σ\'αγαπώ', 'أنا أحبك', 'Ma armastan sind',
     'Аз те обичам', 'Kocham Cię', 'Jeg elsker dig', 'Ich liebe dich',
     'Я тебя люблю', 'Je t\'aime', 'Rakastan sinua', '사랑해', 'Ik hou van je',
     'Já tě miluju', 'Te iubesc', 'Te iubesc', 'Eu te amo', 'あいしてる', 
     'Jag älskar dig', 'Ljubim te', 'ฉันรักเธอนะ', 'Te amo', 'Σ\'αγαπώ', 
     'Љубовта', 'Ti voglio bene', 'عشق', 'Dragoste', 'Anh yêu em', 
     'Renmen', 'ፍቅር', 'munaña', 'Sevgi', 'Љубав', 
-    'karout', 'amà', 'amôr', 'kærleiki', 'mborayhu', 
-    'Upendo', 'sòòyayyàà', 'ljubav', 'Սեր', 'сүю', 
-    'сүйүү', 'tia', 'aroha', 'KHAIR', 'प्रेम', 
-    'kjærlighet', 'munay', 'jecel', 'Kärlek', 'soymek', 
-    'Mahal', 'ярату', 'محبت', 'sopp', 'uthando', 
-    'ความรัก', 'Aşk', 'Tình yêu', 'ליבע'];
+    'karout', 'Es tevi Milu', 'Te sakam', 'Inhobbok', 'Miluji te', 
+    'Volim te', 'chit pa de', 'Khoi huk chau', 'saya Cinta Mu', 'bi chamd hairtai', 
+    'сүйүү', 'Ma tumilai maya garchu', 'Tora dost daram', 'Mahal kita', 'Me do wo', 
+    'Ina sonki', 'Ch\'an Rak Khun', 'Nagligivaget', 'Asavakit', 'e\'g elska tig', 
+    'T Dua Shume', 'Volim Te', 'Ljubim te', 'Jag lskar dig', 'Seni seviyorum', 
+    'ja vas kokhaju', 'Rwy\'n dy garu di', 'ana bayanoo', 'wise cas'];
 const dom = {
     love: document.querySelector('.love')
 }
